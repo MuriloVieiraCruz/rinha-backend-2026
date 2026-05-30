0849f5de2a28a9f4d61a017ea198a75bc3ee954e
murilovieiracruz <murilo12super@gmail.com>
2026-05-30 17:32:57 -0300

    Criado arquivos native image

--- fraud-api/src/main/java/br/com/murilovieira/fraudapi/FraudApiApplication.java
+++ fraud-api/src/main/java/br/com/murilovieira/fraudapi/FraudApiApplication.java
@@ -1,11 +1,14 @@
 package br.com.murilovieira.fraudapi;
 
+import br.com.murilovieira.fraudapi.config.ApplicationHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@ImportRuntimeHints(ApplicationHints.class)
 public class FraudApiApplication {
 
     public static void main(String[] args) {
---
+++ fraud-api/src/main/java/br/com/murilovieira/fraudapi/config/ApplicationHints.java
@@ -0,0 +1,26 @@
+package br.com.murilovieira.fraudapi.config;
+
+import br.com.murilovieira.fraudapi.domain.FraudResponse;
+import br.com.murilovieira.fraudapi.domain.TransactionRequest;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+
+public class ApplicationHints implements RuntimeHintsRegistrar {
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        hints.resources()
+                .registerPattern("normalization.json")
+                .registerPattern("mcc_risk.json");
+
+        hints.reflection()
+                .registerType(TransactionRequest.class,           MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(TransactionRequest.Transaction.class,    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(TransactionRequest.Customer.class,       MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(TransactionRequest.Merchant.class,       MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(TransactionRequest.Terminal.class,       MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(TransactionRequest.LastTransaction.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS)
+                .registerType(FraudResponse.class,                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+    }
+}
